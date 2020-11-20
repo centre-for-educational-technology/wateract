10985a06d6a8cbdb38250191c92cd6aab74ef531
Aili Mets <ayly@tlu.ee>
2020-11-21 00:37:15 +0200

    removed unnecessary blade files

--- resources/views/measurements/show.blade.php
+++
@@ -1,9 +0,0 @@
-
-<div class="card">
-
-    @foreach ($measurement->getFieldValues() as $measurement_value)
-        <label><strong>@lang('springs.measurement_fields.'.$measurement_value->name)</strong></label>
-        <div>{{$measurement_value->value}} {{$measurement_value->unit}}</div>
-    @endforeach
-
-</div>
