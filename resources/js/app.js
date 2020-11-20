f35b733aa8f1abc45479f8ffa6b4555180b4faba
Aili Mets <ayly@tlu.ee>
2020-11-21 00:17:26 +0200

    module cleanup

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,10 +5,28 @@ import Vue from 'vue';
 import { InertiaApp } from '@inertiajs/inertia-vue';
 import { InertiaForm } from 'laravel-jetstream';
 import PortalVue from 'portal-vue';
+import Vuex from 'vuex';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import * as GmapVue from 'gmap-vue';
+import VueLodash from 'vue-lodash';
+import lodash from 'lodash';
 
 Vue.use(InertiaApp);
 Vue.use(InertiaForm);
 Vue.use(PortalVue);
+Vue.use(Vuex)
+Vue.use(ElementUI);
+
+Vue.use(GmapVue, {
+    load: {
+        key: 'google_maps_api_key',
+        libraries: 'places', // This is required if you use the Autocomplete plugin
+        // OR: libraries: 'places,drawing'
+    },
+    installComponents: true
+});
+Vue.use(VueLodash, { name: 'custom' , lodash: lodash })
 
 const app = document.getElementById('app');
 
