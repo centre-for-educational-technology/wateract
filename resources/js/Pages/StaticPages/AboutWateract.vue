a85df1f39974b4875683dfc2536df79704f21f6c
Aili Mets <ayly@tlu.ee>
2021-01-25 16:02:06 +0200

    static pages

---
+++ resources/js/Pages/StaticPages/AboutWateract.vue
@@ -0,0 +1,23 @@
+<template>
+    <app-layout>
+
+        <template #header>
+            <h1>
+                {{ $t('springs.about_wateract') }}
+            </h1>
+        </template>
+
+
+
+    </app-layout>
+</template>
+
+<script>
+import AppLayout from './../../Layouts/AppLayout'
+
+export default {
+    components: {
+        AppLayout,
+    },
+}
+</script>
---
+++ resources/js/Pages/StaticPages/Instructions.vue
@@ -0,0 +1,26 @@
+<template>
+    <app-layout>
+
+        <template #header>
+            <h1>
+                {{ $t('springs.instructions') }}
+            </h1>
+        </template>
+
+        <instructions-et v-if="$i18n.locale == 'et'"></instructions-et>
+
+    </app-layout>
+</template>
+
+<script>
+import AppLayout from './../../Layouts/AppLayout'
+import InstructionsEt from "./et/Instructions";
+
+
+export default {
+    components: {
+        AppLayout,
+        InstructionsEt,
+    },
+}
+</script>
