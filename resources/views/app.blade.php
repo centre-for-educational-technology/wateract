ee1184516f2c6da7c13907caf8f820537b35c1bf
Aili Mets <ayly@tlu.ee>
2021-03-02 11:43:46 +0200

    closes #72, favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,14 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
+        <link rel="apple-touch-icon" type="image/png" sizes="180x180" href="/images/favicons/apple-touch-icon.png">
+        <link rel="android-chrome-icon" type="image/png" sizes="192x192" href="/images/favicons/android-chrome-192x192.png">
+        <link rel="android-chrome-icon" type="image/png" sizes="512x512" href="/images/favicons/android-chrome-512x512.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicons/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/favicons/favicon-16x16.png">
+        <link rel="icon" href="/images/favicons/favicon.ico">
+        <link rel="manifest" href="/images/favicons/site.webmanifest">
+
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
