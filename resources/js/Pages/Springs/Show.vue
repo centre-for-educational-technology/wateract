9db6528a445bd1a7e6c5140d02881de9f871dbd9
Aili Mets <ayly@tlu.ee>
2021-12-14 14:56:31 +0200

    references #18, fb share button

--- resources/js/Pages/Springs/Show.vue
+++ resources/js/Pages/Springs/Show.vue
@@ -69,6 +69,10 @@
                                 </table>
                             </div>
 
+                            <div class="py-2 fb-share-button" :data-href="'/springs/'+spring.code" data-layout="button_count" data-size="small">
+                                <a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore"></a>
+                            </div>
+
                         </div>
 
                         <div class="sm:w-1/4 px-2">
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -49,6 +49,8 @@
         <!-- End Facebook Pixel Code -->
     </head>
     <body class="font-sans antialiased">
+        <div id="fb-root"></div>
+        <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v12.0&appId=894588961418830&autoLogAppEvents=1" nonce="8TeUUaqb"></script>
         @inertia
     </body>
 </html>
