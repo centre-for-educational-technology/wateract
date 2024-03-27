44b9e1c97bb4bb84efba48c7f5699f6068b6f488
GlebRed <glebredko@gmail.com>
2024-03-27 14:38:00 +0000

    Debugging slow load time

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -37,11 +37,11 @@ class Handler extends ExceptionHandler
 
     public function report(\Throwable $exception)
     {
-        if (app()->bound('sentry') && $this->shouldReport($exception)) {
-            app('sentry')->captureException($exception);
-        }
-
-        parent::report($exception);
+//        if (app()->bound('sentry') && $this->shouldReport($exception)) {
+//            app('sentry')->captureException($exception);
+//        }
+//
+//        parent::report($exception);
     }
 
 }
